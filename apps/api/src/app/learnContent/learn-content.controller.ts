6bd96494256c78208424aedfc5fdfca037c2309d
VagnerSilva <vagner23silva@gmail.com>
2021-09-07 21:28:43 -0300

    corrigir import no modulo e no controlador

--- apps/api/src/app/learnContent/learn-content.controller.ts
+++ apps/api/src/app/learnContent/learn-content.controller.ts
@@ -3,12 +3,12 @@ https://docs.nestjs.com/controllers#controllers
 */
 
 import { Controller } from '@nestjs/common';
-import { ContentAreaService } from '../contentarea/content-area.service';
 import { BaseController } from '../utils/base.controller';
+import { LearnContentService } from './learn-content.service';
 
 @Controller()
-export class LearnContentController extends BaseController<ContentAreaService> {
-  constructor(private contentAreaService: ContentAreaService) {
-    super(contentAreaService);
+export class LearnContentController extends BaseController<LearnContentService> {
+  constructor(private learnContentService: LearnContentService) {
+    super(learnContentService);
   }
 }
